642fbaaf2df50fd75d726e25b8048a22c090e280
DylanFahey <dylanfahey@gmail.com>
2020-01-08 23:54:13 -0600

    fixing linter errors

--- hexgram.py
+++ hexgram.py
@@ -14,7 +14,7 @@ import os
 import sys
 import random
 
-ALPHABET = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 '''
 generate
@@ -23,6 +23,8 @@ create word list from dictionary of words which contain center letter and which 
 rules for generation:
 must include at least 1 vowel (not actually necessary)
 '''
+
+
 class GeneratePuzzle():
     # Pick letters for game
     '''
@@ -31,16 +33,16 @@ class GeneratePuzzle():
     random.choices uses replacement
     random.sample makes sure letters don't repeat
     '''
-    letters = random.sample(list(ALPHABET),k=7)
+    letters = random.sample(list(ALPHABET), k=7)
 
     # get the letter in the center
     center_letter = letters[0]
-    print ("center letter is '" + center_letter + "'")
+    print("center letter is '" + center_letter + "'")
 
     # get the letters on the outside
     outside_letters = "".join(letters[1:])
 
-    print ("The other letters are " + outside_letters)
+    print("The other letters are " + outside_letters)
     # for i in letters[1:]:
     #     print(i)
 
@@ -58,7 +60,7 @@ class GeneratePuzzle():
     regex_pattern = "^[" + center_letter + outside_letters + "]*+[" + center_letter + "]+[" + center_letter + outside_letters + "]*$"
     # print (regex_pattern)
 
-    #TODO write to PATTERN_FILE and use fgrep
+    # TODO write to PATTERN_FILE and use fgrep
 
     # create and run grep to make dictionary for current game
     grep_command = "cat " + DICTIONARY_PATH + "| egrep '" + regex_pattern + "' > " + CURRENT_GAME_DICT
@@ -66,9 +68,7 @@ class GeneratePuzzle():
     # run bash command
     os.system(grep_command)
 
-
     # mi amigos a mi escuela me llama 'pinche gringo'
-
     # create text stream and read from game dict
     # with open(CURRENT_GAME_DICT,mode='r') as f:
     #     game_dict = f.readlines()
@@ -83,11 +83,13 @@ class GeneratePuzzle():
     word_list = [line.rstrip('\n') for line in open(CURRENT_GAME_DICT)]
 
     game_list = set(word_list)
-    #TODO check if len(game_list) = 0 and regen if too short
+    # TODO check if len(game_list) = 0 and regen if too short
     # print(game_list)
     # print(len(game_list))
 
 # Generate a puzzle of a certain length
+
+
 game_list = set()
 while len(game_list) < 2:
     game_list = GeneratePuzzle.game_list
@@ -96,23 +98,23 @@ correct_list = set()
 guess_list = set()
 
 
-#run while there are words that haven't been found
+# run while there are words that haven't been found
 while len(game_list) > 0:
     guess = input("guess a word: ")
     if guess in guess_list:
-        print (guess + " has already been guessed")
+        print(guess + " has already been guessed")
     else:
         guess_list.add(guess)
         if guess in game_list:
-            print (guess + " is a word!")
+            print(guess + " is a word!")
             game_list.remove(guess)
         else:
-            #TODO point out what is wrong:
+            # TODO point out what is wrong:
             # not in Dictionary
             # must have at least 4 Letters
             # must contain $center_letter
             # $letter is not in list
-            print (guess + " is not a correct answer")
-print ("game over, you won!")
-#TODO Check validity of puzzle
-#TODO Check for at least on pangram
+            print(guess + " is not a correct answer")
+print("game over, you won!")
+# TODO Check validity of puzzle
+# TODO Check for at least on pangram
